ee410b32e3c35bb655c2a918b2daeba363034b80
Jatin Katyal <jatin.katyal13@gmail.com>
2019-10-06 12:19:49 +0530

    final class

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,81 @@
-from flask import Flask
+#! /usr/bin/python
+from flask import Flask, render_template, request, redirect
+from scrap import scrap_snapdeal
 
 app = Flask("My first application")
 
+USERS = [
+  {
+    "name": "Jatin Katyal",
+    "language": "Python",
+    "state": "Delhi"
+  },
+  {
+    "name": "Ashutosh",
+    "language": "JS",
+    "state": "Mumbai"
+  },
+  {
+    "name": "Sakshi",
+    "language": "C++",
+    "state": "Bangalore"
+  }
+]
+
+FEEDBACKS = []
+
+ABOUT_ME = {
+  "name": "Jatin Katyal",
+  "languages": [
+    "Python",
+    "C++",
+    "GO"
+  ],
+  "company": "Coding Blocks"
+}
+
+@app.route('/scrap', methods = ['GET', 'POST'])
+def scrap():
+  if request.method == "POST":
+    query = request.form['query']
+    result = scrap_snapdeal(query)
+    return render_template('scrap.html', result = result)  
+  return render_template('scrap.html')
+
+@app.route('/form', methods = ['GET', 'POST'])
+def form():
+  if request.method == 'POST':
+    FEEDBACKS.append(request.form)
+    # return redirect('/feedbacks')
+    return "Thank you for submitting feedback"
+  return render_template('form.html')
+
+@app.route('/feedbacks')
+def feedbacks():
+  return render_template('feedback.html', feedbacks = FEEDBACKS)
+
+@app.route('/portfolio')
+def portfolio():
+  return render_template('portfolio.html', **ABOUT_ME)
+
+# Parameterized URLs
+@app.route('/users/<int:id>/<detail>')
+def userDetail(id, detail):
+  user = USERS[id - 1]
+  return user[detail]
+
+@app.route('/users/<int:id>')
+def users(id):
+  user = USERS[id-1]
+  return "Name: {}, Language: {}, State: {}".format(
+    user['name'], 
+    user['language'], 
+    user['state']
+  )
+
 @app.route('/')
 def index():
-  return "hello world !"
+  return render_template('index.html', name = "jatin", marks = [1, 2, 3, 4])
 
 @app.route('/languages')
 def languages(): 
--- flask_app/scrap.py
+++ flask_app/scrap.py
@@ -1,7 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrap_amazon():
-  # Scrapping code
+def scrap_snapdeal(query):
+  url = "https://www.snapdeal.com/search"
+  params = {
+      "keyword": query
+  }
+  r = requests.get(url, params=params)
+  soup = BeautifulSoup(r.content)
+  products = soup.findAll('div', attrs = {"class": "product-tuple-listing"})
 
-  # return string
+  result = []
+
+  for product in products:
+    name = product.find('p', attrs={"class": "product-title"}).text
+    img = product.find('img')
+    if 'src' in img.attrs:
+        image = img.attrs['src']
+    else:
+        image = img.attrs['data-src']
+    
+    result.append((name, image))
+
+  return result
